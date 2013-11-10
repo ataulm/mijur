78d6ff969412872cda7299da115ef0b675361db4
Ataul Munim <timberwo7ves@gmail.com>
2013-11-10 23:18:11 +0000

    Updates Imgur class with correct constant values

--- core/src/main/java/uk/co/ataulm/imgur/core/Imgur.java
+++ core/src/main/java/uk/co/ataulm/imgur/core/Imgur.java
@@ -1,5 +1,6 @@
 package uk.co.ataulm.imgur.core;
 
+import retrofit.RestAdapter;
 import retrofit.http.GET;
 import retrofit.http.Path;
 
@@ -10,19 +11,16 @@ public class Imgur {
     interface Api {
         static final String API_URL = "https://api.imgur.com/3/";
         static final String CLIENT_ID = ApiConstants.API_CLIENT_ID;
-        static final String CLIENT_SECRET = "what";//BuildConfig.API_CLIENT_SECRET;
+        static final String CLIENT_SECRET = ApiConstants.API_CLIENT_SECRET;
 
         @GET("/image/{id}")
         Image image(@Path("id") String id);
     }
 
     public static void main(String... args) {
-        System.out.println("client id = " + Api.CLIENT_ID);
-        System.out.println("client secret = " + Api.CLIENT_SECRET);
+        RestAdapter restAdapter = new RestAdapter.Builder().setServer(Api.API_URL).build();
 
-//        RestAdapter restAdapter = new RestAdapter.Builder().setServer(Api.API_URL).build();
-//
-//        Api imgur = restAdapter.create(Api.class);
-//        Image returnedImage = imgur.image("CoSS30e");
+        Api imgur = restAdapter.create(Api.class);
+        Image returnedImage = imgur.image("CoSS30e");
     }
 }
