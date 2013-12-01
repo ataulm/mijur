8332d6b4678d73b4b8475dcaceb8529f7335bd78
Ataul Munim <timberwo7ves@gmail.com>
2013-12-01 00:01:08 +0000

    Adds util to ImgurAuth to authorise requests with client id
    - The test added fails - it's unable to set properties on an open connection
    - I'm unsure how it's different from: https://github.com/talklittle/ImgurAPIv3ExampleAndroid/blob/master/ImgurAPIv3Example/src/main/java/com/imgur/api3example/login/ImgurAuthorization.java#L86, which works

--- core/src/test/java/uk/co/ataulm/mijur/core/api/ImgurAuthShould.java
+++ core/src/test/java/uk/co/ataulm/mijur/core/api/ImgurAuthShould.java
@@ -1,17 +1,40 @@
 package uk.co.ataulm.mijur.core.api;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class ImgurAuthShould {
+    private static final String PROPERTY_KEY_AUTHORISATION = "Authorization";
+    private static final String AUTHORISATION_ENDPOINT = "https://api.imgur.com/oauth2/token";
+    private static final String PROPERTY_VALUE_CLIENT_ID_PREFIX = "Client-ID ";
 
     @Test
     public void retain_a_single_instance() throws Exception {
-        ImgurAuthApi one = ImgurAuthApi.getInstance();
-        ImgurAuthApi two = ImgurAuthApi.getInstance();
+        ImgurAuth one = ImgurAuth.getInstance();
+        ImgurAuth two = ImgurAuth.getInstance();
 
         assertThat(one == two, is(true));
+    }
+
+    // TODO: investigate
+    @Ignore("Fails: cannot add properties when open connection, but ImgurAPIv4Example does somehow...")
+    @Test
+    public void add_client_id_to_http_connections_when_no_access_token_available() throws Exception {
+        HttpURLConnection connection = (HttpURLConnection) new URL(AUTHORISATION_ENDPOINT).openConnection();
+        connection.setDoOutput(true);
+
+        ImgurAuth.addAuthorisationHeadersTo(connection);
+
+        String expected = PROPERTY_VALUE_CLIENT_ID_PREFIX + ApiConstants.API_CLIENT_ID;
+        String actual = connection.getRequestProperty(PROPERTY_KEY_AUTHORISATION);
+        assertThat(expected.equals(actual), is(true));
     }
 }
