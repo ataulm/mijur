b319afcaf87a715343d3af7b233db2e6644a230e
Ataul Munim <timberwo7ves@gmail.com>
2013-12-15 13:25:25 +0000

    Reduces Imgur from implementing unrelated interface, choosing anonymous class instead

--- core/src/main/java/uk/co/ataulm/mijur/core/api/Imgur.java
+++ core/src/main/java/uk/co/ataulm/mijur/core/api/Imgur.java
@@ -6,13 +6,13 @@ import retrofit.RestAdapter;
 /**
  * Attempts OAuth 2.0 authentication without use of Scribe
  */
-public class Imgur implements RequestInterceptor {
+public class Imgur {
     private static final String PROPERTY_KEY_AUTHORISATION = "Authorization";
     private static final String PROPERTY_VALUE_CLIENT_ID_PREFIX = "Client-ID ";
     private static final String API_URL = "https://api.imgur.com/3";
 
     private static Imgur imgur;
-    private static ImgurApi imgurRestAdapter;
+    private static ImgurApi imgurApi;
 
     private Imgur() {
     }
@@ -21,25 +21,24 @@ public class Imgur implements RequestInterceptor {
         if (imgur == null) {
             setupImgurClient();
         }
-        return imgurRestAdapter;
+        return imgurApi;
     }
 
     private static void setupImgurClient() {
         imgur = new Imgur();
         RestAdapter adapter = new RestAdapter.Builder()
                 .setServer(API_URL)
-                .setRequestInterceptor(imgur)
-                .build();
-        imgurRestAdapter = adapter.create(ImgurApi.class);
+                .setRequestInterceptor(new RequestInterceptor() {
+                    @Override
+                    public void intercept(RequestFacade request) {
+                        request.addHeader(PROPERTY_KEY_AUTHORISATION, PROPERTY_VALUE_CLIENT_ID_PREFIX + ApiConstants.API_CLIENT_ID);
+                    }
+                }).build();
+        imgurApi = adapter.create(ImgurApi.class);
     }
 
     public static ImageResponse getImageWith(String id) {
         return instance().getImage(id);
-    }
-
-    @Override
-    public void intercept(RequestFacade request) {
-        request.addHeader(PROPERTY_KEY_AUTHORISATION, PROPERTY_VALUE_CLIENT_ID_PREFIX + ApiConstants.API_CLIENT_ID);
     }
 
 }
