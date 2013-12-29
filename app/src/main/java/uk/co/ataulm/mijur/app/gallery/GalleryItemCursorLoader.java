9e8fecb8208855651749d45cbcb2499dd80bd996
Ataul Munim <timberwo7ves@gmail.com>
2013-12-29 02:06:40 +0000

    Adds row offset (1) to gallery, so we can display 1st item as header

--- app/src/main/java/uk/co/ataulm/mijur/app/gallery/GalleryItemCursorLoader.java
+++ app/src/main/java/uk/co/ataulm/mijur/app/gallery/GalleryItemCursorLoader.java
@@ -8,14 +8,16 @@ import uk.co.ataulm.mijur.app.Provider;
 class GalleryItemCursorLoader extends CursorLoader {
 
     private static final String FALSE = "0";
+    private static final String LIMIT_KEY = "limit";
+    private static final String ROW_OFFSET = "1";
+    private static final String NO_ROW_LIMIT = "999999";
 
     public GalleryItemCursorLoader(Context context) {
-        super(context,
-                Provider.Uris.GALLERY_ITEM.uri(),
-                null,
-                Provider.GalleryItem.IS_ANIMATED + " = ? AND " + Provider.GalleryItem.IS_ALBUM + " = ?",
-                new String[]{FALSE, FALSE},
-                Provider.GalleryItem.SUBMISSION_DATETIME.toString() + " DESC");
+        super(context);
+        setUri(Provider.Uris.GALLERY_ITEM.uri().buildUpon().appendQueryParameter(LIMIT_KEY, ROW_OFFSET + "," + NO_ROW_LIMIT).build());
+        setSelection(Provider.GalleryItem.IS_ANIMATED + " = ? AND " + Provider.GalleryItem.IS_ALBUM + " = ?");
+        setSelectionArgs(new String[]{FALSE, FALSE});
+        setSortOrder(Provider.GalleryItem.SUBMISSION_DATETIME.toString() + " DESC");
     }
 
 }
