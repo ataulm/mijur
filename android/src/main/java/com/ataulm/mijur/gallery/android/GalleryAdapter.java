d0b9fbfc8da886439919dad45c27099ce4679f02
Ataul Munim <timberwo7ves@gmail.com>
2014-03-23 22:22:38 +0000

    Adds second style for gallery item view

--- android/src/main/java/com/ataulm/mijur/gallery/android/GalleryAdapter.java
+++ android/src/main/java/com/ataulm/mijur/gallery/android/GalleryAdapter.java
@@ -4,24 +4,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ataulm.mijur.R;
 import com.ataulm.mijur.base.android.MijurAdapter;
 import com.ataulm.mijur.gallery.GalleryItem;
-
-import com.ataulm.mijur.R;
 
 class GalleryAdapter extends MijurAdapter<GalleryItem> {
 
     @Override
     public View getView(int position, View view, ViewGroup parent) {
         if (view == null) {
-            view = createView(LayoutInflater.from(parent.getContext()));
+            view = createView(LayoutInflater.from(parent.getContext()), position);
         }
         ((GalleryItemView) view).updateWith(getItem(position));
         return view;
     }
 
-    private View createView(LayoutInflater inflater) {
-        return inflater.inflate(R.layout.gallery_item_view, null, false);
+    private View createView(LayoutInflater inflater, int position) {
+        if (position % 2 == 0) {
+            return inflater.inflate(R.layout.gallery_item_view_1, null, false);
+        }
+        return inflater.inflate(R.layout.gallery_item_view_2, null, false);
     }
 
 }
