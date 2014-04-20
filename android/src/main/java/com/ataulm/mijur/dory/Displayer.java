c624ec31cf45321a5b415dae747054054b549b95
Ataul Munim <timberwo7ves@gmail.com>
2014-04-20 16:43:44 +0100

    Adds javadoc for public interfaces

--- android/src/main/java/com/ataulm/mijur/dory/Displayer.java
+++ android/src/main/java/com/ataulm/mijur/dory/Displayer.java
@@ -4,6 +4,9 @@ import android.view.View;
 
 public interface Displayer<T, U extends View> {
 
+    /**
+     * Displays the content in the View.
+     */
     void display(T content, U view);
 
 }
--- android/src/main/java/com/ataulm/mijur/dory/StreamConverter.java
+++ android/src/main/java/com/ataulm/mijur/dory/StreamConverter.java
@@ -6,6 +6,11 @@ import rx.Observable;
 
 public interface StreamConverter<T> {
 
+    /**
+     * Converts an input stream into an object of type T.
+     *
+     * The width and height are the desired dimensions of the output.
+     */
     Observable<T> observableConverting(InputStream stream, int width, int height);
 
 }
