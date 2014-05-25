69fd66ccc79dd87703b504b15a06103ad3c3b6ef
Ataul Munim <timberwo7ves@gmail.com>
2014-05-25 18:57:30 +0100

    Adds a parser to retrieve to flatten a tree of comments into a list

---
+++ android/src/main/java/com/ataulm/mijur/data/parser/CommentParser.java
@@ -0,0 +1,70 @@
+package com.ataulm.mijur.data.parser;
+
+import com.ataulm.mijur.data.Comment;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CommentParser {
+
+    private final Gson gson;
+
+    private CommentParser(Gson gson) {
+        this.gson = gson;
+    }
+
+    public static CommentParser newInstance() {
+        return new CommentParser(new Gson());
+    }
+
+    public List<Comment> parse(InputStream stream) {
+        JsonReader reader = new JsonReader(new InputStreamReader(stream));
+        GsonCommentResponse gsonCommentResponse = gson.fromJson(reader, GsonCommentResponse.class);
+        return parse(gsonCommentResponse);
+    }
+
+    private List<Comment> parse(GsonCommentResponse gsonCommentResponse) {
+        if (gsonCommentResponse.success) {
+            return parseAllComments(gsonCommentResponse.data);
+        }
+        return Collections.emptyList();
+    }
+
+    /**
+     * Parses all comments into a flat List<Comment>.
+     * <p/>
+     * If a comment has a reply, they will be siblings in this list.
+     */
+    private List<Comment> parseAllComments(List<GsonComment> gsonComments) {
+        // initial size is naive; assumes no replies but it's best case scenario
+        List<Comment> comments = new ArrayList<Comment>(gsonComments.size());
+        for (GsonComment gsonComment : gsonComments) {
+            Comment comment = parseGsonComment(gsonComment);
+            comments.add(comment);
+            if (gsonCommentHasReplies(gsonComment)) {
+                List<Comment> replies = parseAllComments(gsonComment.children);
+                comments.addAll(replies);
+            }
+        }
+        return comments;
+    }
+
+    private boolean gsonCommentHasReplies(GsonComment gsonComment) {
+        return gsonComment.children != null && gsonComment.children.size() > 0;
+    }
+
+    private Comment parseGsonComment(GsonComment gsonComment) {
+        return new Comment(gsonComment.id,
+                gsonComment.author,
+                gsonComment.upvotes,
+                gsonComment.downvotes,
+                gsonComment.text,
+                gsonComment.parentId);
+    }
+
+}
